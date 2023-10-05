2aa8bf71e5453e9c23ca29f4a146198b72df8286
Alexander Rodriguez <30503623+rodri270@users.noreply.github.com>
2023-10-05 09:51:24 -0400

    added all files

---
+++ LizardTime/test.py
@@ -0,0 +1,9 @@
+valuesfromuser = ['2','3']
+
+uservalue = input("Please type value:")
+for i in valuesfromuser:
+    if (uservalue == i):
+        print("values match gg")
+        exit()
+    else:
+        print("values doesn't match")
